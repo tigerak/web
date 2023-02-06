76ceac1e29f322f651e6d2c46a4290138f2a853d
HeoYB <tiger_ak@doinglab.com>
2023-02-06 13:07:36 +0900

    Add require

--- make_data/run_data.py
+++ make_data/run_data.py
@@ -1,19 +1,6 @@
 import streamlit as st
 
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained(
-    'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
-    bos_token='[BOS]', eos_token='[EOS]', unk_token='[UNK]', pad_token='[PAD]', mask_token='[MASK]'
-    )
-
-def decomposition(sentence):
-    input_idx = tokenizer.encode(sentence)
-    output = []
-    for i in input_idx:
-        out = tokenizer.decode(i)
-        output.append(out)
-    return output
+from util.sentence_decomp import decomposition
 
 st.title('데이터 생성기')
 
