8501fa4b8a1c4dba74a2db2e1e7a1e066351b914
tigerak <tigerakyb@gmail.com>
2023-03-15 10:14:47 +0900

    Add api

--- config.py
+++ config.py
@@ -7,6 +7,18 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = \
         os.environ.get('DATABASE_URI')\
-        or 'sqlite:///' + os.path.join(basedir, 'DB.db')
+        or 'sqlite:///' + os.path.join(basedir, 'test_DB.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAX_CONTENT_LENGTH=1024*1024 # 1mb 이상 파일 업로드 금지
+    MAX_CONTENT_LENGTH=1024*1024 
+    
+class Maum_api:
+    part = {
+        "app_id": "83fdc22a-20d1-5fc8-8af8-a6efe6b4af69",
+        "name": "문해력",
+        "item": [
+            "open-api-get-query"
+        ],
+        "param": [
+            "https://e4e7-14-36-34-28.jp.ngrok.io/api"
+        ]
+    }
--- make_data/main/routes.py
+++ make_data/main/routes.py
@@ -2,8 +2,10 @@ from make_data.main import bp
 from make_data.utils import decomposition
 from make_data.models.database import Para_data, Sent_data
 from make_data.extensions import db
+from config import Maum_api
 
 from flask import render_template, request, redirect, url_for
+import requests
 
 @bp.route('/', methods=['GET', 'POST'])
 def decom_but():
@@ -23,6 +25,25 @@ def decom_but():
 
     else:
         return render_template('index.html')
+    
+@bp.route('/api', methods=['GET', 'POST'])
+def api():
+    import json
+    prompt = "다음 글을 따라쓰면서 중요한 단어에는 별표시를 해줘. 그리고 한 문장으로 요약해줘. : "
+    completion = "\n\n\n성소수자 등 성 문제에 대한 인식의 충돌도 이에 해당된다고 볼 수 있다. 최근 국내에서 주목받는 MZ세대의 정치적 지향의 변화에는, ‘정치적 올바름(PC)’에 대한 좌절과 반발이 녹아있다. 앞서 서술한 부동산 문제 등을 둘러싼 사회경제적 좌절과 함께, 민주화시대를 관통하며 작동해온 각종 PC에 대한 정서적 반발이 작동하고 있는 것이다."
+    data = {
+        "prompt" : prompt,
+        "completion" : completion
+    }
+    send = json.dumps(data)
+    return send
+
+@bp.route('/feed', methods=['GET', 'POST'])
+def get_json():
+    ans = Maum_api.part
+    response = requests.post('https://norchestra.maum.ai/harmonize/dosmart', json=ans)
+    text = response.json()
+    return text
 
 @bp.route('/db_test', methods=['GET', 'POST'])
 def sql():
